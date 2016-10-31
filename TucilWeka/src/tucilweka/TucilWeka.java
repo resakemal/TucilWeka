20a27f9716b55772ee7e2e71679c7e7ffddc19cf
resakemal <resakemal@gmail.com>
2016-10-31 21:02:40 +0700

    Load File
    Somehow tidak bisa

--- TucilWeka/src/tucilweka/TucilWeka.java
+++ TucilWeka/src/tucilweka/TucilWeka.java
@@ -4,18 +4,39 @@
  * and open the template in the editor.
  */
 package tucilweka;
+import weka.core.*;
+import weka.core.converters.ArffLoader;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author LENOVO
  */
 public class TucilWeka {
-
+    Instances I;
     /**
      * @param args the command line arguments
      */
+    
+    public void loadFile() throws IOException
+    {
+        ArffLoader loader = new ArffLoader();
+        loader.setSource(new File("iris.arff"));
+        Instances in = loader.getDataSet();
+        I = in;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        TucilWeka W = new TucilWeka();
+        try {
+            W.loadFile();
+        } catch (IOException ex) {
+            Logger.getLogger(TucilWeka.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println(W.I.toSummaryString());
     }
     
 }
